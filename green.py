acab5d9d73b4739fae97d985e07c9bcd4ae82815
SabrinaMarshal <60549458+SabrinaMarshal@users.noreply.github.com>
2020-02-19 20:13:59 +0530

    Add files via upload

---
+++ green.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+while(1):
+     _, frame = cap.read()
+     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+     lower_green = np.array([65,60,60])
+     upper_green = np.array([80,255,255])
+     mask = cv2.inRange(hsv, lower_green, upper_green)
+     kernel = np.ones((5,5),'int')
+     dilated = cv2.dilate(mask,kernel)
+     res = cv2.bitwise_and(frame,frame, mask=mask)
+     ret,thrshed = cv2.threshold(cv2.cvtColor(res,cv2.COLOR_BGR2GRAY),3,255,cv2.THRESH_BINARY)
+     contours,hier = cv2.findContours(thrshed,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+     for cnt in contours:
+         area = cv2.contourArea(cnt)
+         if area >1000:
+             cv2.putText(frame, "Green Object Detected", (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, 1)
+             cv2.rectangle(frame,(5,40),(400,100),(0,255,255),2)
+     cv2.imshow('frame',frame)
+     k = cv2.waitKey(5) & 0xFF
+     if k == 27:
+         break
+cap.release()
+cv2.destroyAllWindows()
