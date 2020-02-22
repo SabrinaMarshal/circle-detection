5a477c1735a5f2ddfe5e51ade689ed8eadbadda2
SabrinaMarshal <60549458+SabrinaMarshal@users.noreply.github.com>
2020-02-22 20:36:01 +0530

    Update greenball.py

--- greenball.py
+++ greenball.py
@@ -13,21 +13,15 @@ ap.add_argument("-v", "--video",
 ap.add_argument("-b", "--buffer", type=int, default=64,
         help="max buffer size")
 args = vars(ap.parse_args())
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
+# define the lower and upper boundaries of the "green" ball in the HSV color space
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
-# if a video path was not supplied, grab the reference
-# to the webcam
 vs = cv2.VideoCapture(0)
-# allow the camera or video file to warm up
-time.sleep(2.0)
 # keep looping
 while True:
         # grab the current frame
         _, frame = vs.read()
-        # handle the frame from VideoCapture or VideoStream
+        # handle the frame from VideoCapture
         frame = frame[1] if args.get("video", False) else frame
         # if we are viewing a video and we did not grab a frame,
         # then we have reached the end of the video
@@ -60,14 +54,38 @@ while True:
                 M = cv2.moments(c)
                 center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
                 # only proceed if the radius meets a minimum size
-                if radius > 10:
+                if radius > 20:
                         # draw the circle and centroid on the frame,
                         # then update the list of tracked points
                         cv2.circle(frame, (int(x), int(y)), int(radius),
                                 (0, 255, 255), 2)
                         cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                        cv2.putText(frame, "Green ball detected", (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, 1)
-                        cv2.rectangle(frame,(5,40),(400,100),(0,255,255),2)
+                        print("Green ball detected")
+                        res = cv2.bitwise_and(frame,frame, mask=mask)
+                        ret,thrshed = cv2.threshold(cv2.cvtColor(res,cv2.COLOR_BGR2GRAY),3,255,cv2.THRESH_BINARY)
+                        contours,hier = cv2.findContours(thrshed,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+                        cnt=max(contours,key=cv2.contourArea)
+                        area = cv2.contourArea(cnt)
+                        # Calculate whole area
+                        _, img=vs.read()
+                        h,w = img.shape[:2]
+                        whole_area_mask = np.ones((h, w), np.uint8)
+                        ret, thresh = cv2.threshold(whole_area_mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+                        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+                        cnts = max(contours, key=cv2.contourArea)
+                        whole_area = cv2.contourArea(cnts)
+                        perc=(area*100/whole_area)
+                        print("Percentage of ball: ",perc)
+                        if(center[0]<=300 and center[1]<=200):
+                                print("Top left corner")
+                        elif(center[0]>=300 and center[1]<=200):
+                                print("Top right corner")
+                        elif(center[0]<=300 and center[1]>=200):
+                                print("Bottom left corner")
+                        elif(center[0]>=300 and center[1]>=200):
+                                print("Bottom right corner")
+                        cv2.waitKey(-1)
+                        break
         # show the frame to our screen
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
